058043041629126526736e570d70452fb7bc9c07
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-11-16 20:46:21 -0500

    exercise 1 skeleton. work in progress

---
+++ small_town_teller.py
@@ -0,0 +1,35 @@
+class Person:
+
+    def __init__(self, id, first_name, last_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+
+class Account:
+
+    def __init__(self, number, type, owner, balance):
+        self.number = number
+        self.type = type
+        self.owner = owner
+        self.balance = balance
+
+class Bank:
+    def addCustomer():
+        pass
+
+    def addAccount():
+        pass
+
+    def removeAccount():
+        pass
+
+    def deposit():
+        pass
+
+    def withdraw():
+        pass
+
+    def balance():
+        pass
+
+
